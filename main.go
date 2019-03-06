d232547f6ddcc8e1ff9c8627dc73cb5925d20c62
rewenset <a.zakurenyi@gmail.com>
2019-03-06 16:36:42 +0200

    add two more handlers. They are dummies for now

--- main.go
+++ main.go
@@ -11,9 +11,22 @@ import (
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", index)
+	router.HandleFunc("/authors", authorIndex)
+	router.HandleFunc("/authors/{authorID}", authorShow)
+
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Coming soon...")
+	fmt.Fprint(w, "Empty index page")
+}
+
+func authorIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Show all authors")
+}
+
+func authorShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	authorID := vars["authorID"]
+	fmt.Fprintf(w, "Show Author with ID %s", authorID)
 }
